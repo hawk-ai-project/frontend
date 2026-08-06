5f0ee5b103cd765da101a5abc1b734afa50096c1
ejjang2030 <ejjang2030@gmail.com>
2026-08-06 16:27:00 +0900

    fix : data-scroll-behavior 추가

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="ko">
+    <html lang="ko" data-scroll-behavior="smooth">
       <body>
         <AuthProvider>
           <Header />
