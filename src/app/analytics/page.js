7d459067928db8b874e652449142213d0a53f9f5
DONGKEUN <dklee983@gmail.com>
2026-08-11 12:34:45 +0900

    feat: 통계 페이지 레이아웃 수정

--- src/app/analytics/page.js
+++ src/app/analytics/page.js
@@ -1,14 +1,11 @@
-// import './analytics.css';
 import AnalyticsClient from '@/components/analytics/AnalyticsClient';
 
 export const metadata = { title: '통계 분석' };
 
 export default function AnalyticsPage() {
   return (
-    <div className="analytics-page-wrapper">
-      <div className="analytics-container">
-        <AnalyticsClient />
-      </div>
+    <div className="page-shell">
+      <AnalyticsClient />
     </div>
   );
 }
