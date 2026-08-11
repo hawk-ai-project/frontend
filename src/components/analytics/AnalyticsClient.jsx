21345fd739a73209bd6b7e1bbc93190ff039122b
DDORINY <jihun22400669@gmail.com>
2026-08-11 11:13:38 +0900

    feat: 통계 데이터 인사이트 UI 연결

--- src/components/analytics/AnalyticsClient.jsx
+++ src/components/analytics/AnalyticsClient.jsx
@@ -8,7 +8,7 @@ import { getApiErrorMessage } from "@/services/apiClient";
 import AnalyticsHeader from "./AnalyticsHeader";
 import AnalyticsSummaryCards from "./AnalyticsSummaryCards";
 import AnalyticsCharts from "./AnalyticsCharts";
-import AnalyticsInsights from "./AnalyticsInsights";
+import AIAnalyticsInsights from "./AIAnalyticsInsights";
 
 export default function AnalyticsClient() {
   const [startDate, setStartDate] = useState("2026-08-01");
@@ -201,7 +201,11 @@ export default function AnalyticsClient() {
             trends={data.trends}
             distribution={data.distribution}
           />
-          <AnalyticsInsights insights={data.insights} />
+          <AIAnalyticsInsights
+            key={`${query.startDate}-${query.endDate}-${query.locationId || "all"}`}
+            analytics={data}
+            query={query}
+          />
         </>
       )}
     </div>
